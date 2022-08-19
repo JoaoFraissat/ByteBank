b6a11f6f20779d44145e206a9f72f1f71cc2a010
João Victor <j.victor_2000@hotmail.com>
2022-08-19 04:40:24 -0300

    código

---
+++ ByteBank/GerenciadorBonificacao.cs
@@ -0,0 +1,24 @@
+﻿using ByteBank.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    public class GerenciadorBonificacao
+    {
+        private double _totalbonificacao;
+
+        public void Registrar(Funcionario funcionario)
+        {
+            _totalbonificacao += funcionario.GetBonificacao();
+        }
+
+        public double GetTotalBonificacao()
+        {
+            return _totalbonificacao;
+        }
+    }
+}
